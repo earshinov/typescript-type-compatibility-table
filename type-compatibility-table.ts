31e2960495211f8a7962deed5be691c92735f5d1
Evgeny Arshinov <earshinov@gmail.com>
2019-03-10 19:05:25 +0300

    Add `unknown`, thanks to @Hoten

--- type-compatibility-table.ts
+++ type-compatibility-table.ts
@@ -8,6 +8,7 @@ var u: undefined = undefined;
 var v: void = fv();
 var n: null = null;
 var t: {} = {}; // random type
+var k: unknown = {};
 var a: any = {};
 
 /*
@@ -17,23 +18,25 @@ y |x = y| (typeof X "assignable from" typeof Y)
 --+-----+
 */
 
-/*         | never        | undefined    | void         | null         | T            | any          |
------------+--------------+--------------+--------------+--------------+--------------+--------------+
-never      |*/  x = x;  /*|*/  u = x;  /*|*/  v = x;  /*|*/  n = x;  /*|*/  t = x;  /*|*/  a = x;  /*|
-undefined  |*/  x = u;  /*|*/  u = u;  /*|*/  v = u;  /*|*/  n = u;  /*|*/  t = u;  /*|*/  a = u;  /*|
-void       |*/  x = v;  /*|*/  u = v;  /*|*/  v = v;  /*|*/  n = v;  /*|*/  t = v;  /*|*/  a = v;  /*|
-null       |*/  x = n;  /*|*/  u = n;  /*|*/  v = n;  /*|*/  n = n;  /*|*/  t = n;  /*|*/  a = n;  /*|
-T          |*/  x = t;  /*|*/  u = t;  /*|*/  v = t;  /*|*/  n = t;  /*|*/  t = t;  /*|*/  a = t;  /*|
-any        |*/  x = a;  /*|*/  u = a;  /*|*/  v = a;  /*|*/  n = a;  /*|*/  t = a;  /*|*/  a = a;  /*|
------------+--------------+--------------+--------------+--------------+--------------+--------------+
+/*         | never        | undefined    | void         | null         | T            | unknown      | any          |
+-----------+--------------+--------------+--------------+--------------+--------------+--------------+--------------+
+never      |*/  x = x;  /*|*/  u = x;  /*|*/  v = x;  /*|*/  n = x;  /*|*/  t = x;  /*|*/  k = x;  /*|*/  a = x;  /*|
+undefined  |*/  x = u;  /*|*/  u = u;  /*|*/  v = u;  /*|*/  n = u;  /*|*/  t = u;  /*|*/  k = u;  /*|*/  a = u;  /*|
+void       |*/  x = v;  /*|*/  u = v;  /*|*/  v = v;  /*|*/  n = v;  /*|*/  t = v;  /*|*/  k = v;  /*|*/  a = v;  /*|
+null       |*/  x = n;  /*|*/  u = n;  /*|*/  v = n;  /*|*/  n = n;  /*|*/  t = n;  /*|*/  k = n;  /*|*/  a = n;  /*|
+T          |*/  x = t;  /*|*/  u = t;  /*|*/  v = t;  /*|*/  n = t;  /*|*/  t = t;  /*|*/  k = t;  /*|*/  a = t;  /*|
+unknown    |*/  x = k;  /*|*/  u = k;  /*|*/  v = k;  /*|*/  n = k;  /*|*/  t = k;  /*|*/  k = k;  /*|*/  a = k;  /*|
+any        |*/  x = a;  /*|*/  u = a;  /*|*/  v = a;  /*|*/  n = a;  /*|*/  t = a;  /*|*/  k = a;  /*|*/  a = a;  /*|
+-----------+--------------+--------------+--------------+--------------+--------------+--------------+--------------+
 */
 
 // Observations (under `strict: true`):
 //
-//   1. `never` assignable-from only `never`
-//   2. `never` assignable-to any type
-//   3. `any` assignable-from any type
-//   4. `any` assignable-to any type (except for `never`, see p.1)
-//   5. `undefined` assignable-to `void`
-//   6. `undefined`, `null`, `T` are incompatible
+//   1. `never` is assignable-from only `never`
+//   2. `never` is assignable-to any type
+//   3. `any` and `unknown` are assignable-from any type
+//   4. `unknown` is assignable-to only `any` and `unknown`
+//   5. `any` is assignable-to any type (except for `never`, see p.1)
+//   6. `undefined` is assignable-to `void`
+//   7. `undefined`, `null`, `T` are incompatible
 //
